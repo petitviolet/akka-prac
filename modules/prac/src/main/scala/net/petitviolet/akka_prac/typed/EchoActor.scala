d7527778d9a1815c1b873e53551d1e4c62858ba1
petitviolet <violethero0820@gmail.com>
2018-08-08 21:21:44 +0900

    typed actor creation with systemActorOf

--- modules/prac/src/main/scala/net/petitviolet/akka_prac/typed/EchoActor.scala
+++ modules/prac/src/main/scala/net/petitviolet/akka_prac/typed/EchoActor.scala
@@ -25,14 +25,15 @@ object EchoActor {
 }
 
 object EchoActorMain extends App {
+  private implicit val timeout = Timeout(5.seconds)
   case class Start(msg: String)
   val start = Behaviors.setup[Start] { ctx =>
+    implicit val scheduler = ctx.system.scheduler
+    implicit val ec = ctx.executionContext
+
     val echo: ActorRef[EchoActor.Message] = ctx.spawn(EchoActor.echo, "echo")
 
     Behaviors.receiveMessage { case Start(message) =>
-      implicit val timeout = Timeout(5.seconds)
-      implicit val scheduler = ctx.system.scheduler
-      implicit val ec = ctx.executionContext
       val reply: Future[EchoActor.Reply] = echo ? { ref => EchoActor.Message(message, ref) }
       reply.onComplete {
         case Success(EchoActor.Reply(value, from)) => ctx.log.info(s"reply success: {}. from: {}", value, from.path)
@@ -43,9 +44,21 @@ object EchoActorMain extends App {
   }
 
   val system = ActorSystem(start, "EchoActorMain")
+  implicit val ec = system.executionContext
+  implicit val scheduler = system.scheduler
 
   val msg = scala.io.StdIn.readLine("Input > ")
   system ! Start(msg)
 
-  concurrent.Await.ready(system.terminate(), 3.seconds)
+  val replyF: Future[EchoActor.Reply] = system.systemActorOf(EchoActor.echo, "echoActor") flatMap {
+    echoActor: ActorRef[EchoActor.Message] =>
+      val msg = scala.io.StdIn.readLine("Input > ")
+      echoActor ? { self => EchoActor.Message(msg, self) }
+  }
+  replyF onComplete {
+    case Success(EchoActor.Reply(value, from)) => system.log.info(s"reply success: {}. from: {}", value, from.path)
+    case Failure(exception) => system.log.error("reply fail: {}", exception.getMessage)
+  }
+
+  concurrent.Await.ready(replyF flatMap { _ =>system.terminate() }, 3.seconds)
 }
