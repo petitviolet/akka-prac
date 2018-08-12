b8d6b66e544749736da823cc7fab993f779e65e4
petitviolet <violethero0820@gmail.com>
2018-08-12 23:31:07 +0900

    modified CountUpActor

--- modules/prac/src/main/scala/net/petitviolet/akka_prac/typed/CountUpActor.scala
+++ modules/prac/src/main/scala/net/petitviolet/akka_prac/typed/CountUpActor.scala
@@ -3,10 +3,15 @@ package net.petitviolet.akka_prac.typed
 import akka.NotUsed
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
+import akka.actor.typed.scaladsl.AskPattern.Askable
+import akka.util.Timeout
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 object CountUpActor extends App {
+  private implicit val timeout = Timeout(5.seconds)
+
   sealed trait Command
   case class Plus(num: Int) extends Command
   case object Increment extends Command
@@ -15,16 +20,13 @@ object CountUpActor extends App {
 
   case class Count(value: Int)
 
-  private def actor(current: Int = 0): Behavior[Command] = Behaviors.receive[Command] {
-    (ctx, msg) =>
-      msg match {
-        case Increment => actor(current + 1)
-        case Plus(num) => actor(current + num)
-        case Minus(num) => actor(current - num)
-        case TellMe(from) =>
-          from ! Count(current)
-          Behaviors.same
-      }
+  private def actor(current: Int = 0): Behavior[Command] = Behaviors.receiveMessage[Command] {
+    case Increment => actor(current + 1)
+    case Plus(num) => actor(current + num)
+    case Minus(num) => actor(current - num)
+    case TellMe(from) =>
+      from ! Count(current)
+      Behaviors.same
   }
 
   private def teller = Behaviors.receive[Count] { case (ctx, Count(value)) =>
@@ -33,13 +35,17 @@ object CountUpActor extends App {
   }
 
   val main: Behavior[NotUsed] = Behaviors.setup { ctx =>
-    val sender = ctx.spawn(teller, "teller")
+    implicit val scheduler = ctx.system.scheduler
+    implicit val ec = ctx.executionContext
+
+    val sender: ActorRef[Count] = ctx.spawn(teller, "teller")
     Behaviors.receiveMessage { _ =>
-      val count = ctx.spawn(actor(), "countUp")
+      val count: ActorRef[Command] = ctx.spawn(actor(), "countUp")
       count ! Plus(10)
       count ! Increment
       count ! Minus(5)
       count ! TellMe(sender)
+      val x: Future[Count] = count ? { ref => TellMe(ref) }
       Behaviors.same
     }
   }
